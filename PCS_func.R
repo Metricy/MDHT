a7c668243d2e362a3e1286bde7c132fef6bcdb04
小姐姐的憂鬱貓 <32256102+EricGraecum@users.noreply.github.com>
2017-09-26 22:41:14 +0800

    Update PCS_func.R

--- PCS_func.R
+++ PCS_func.R
@@ -47,3 +47,58 @@ test <- function(lo,up,sep,dat,method='Holm`s step-down') {
   }
   res<-min(c(na.omit(p_vals)))
   return(list(res,method))}
+
+
+do_test <- function(pcs=T,ks=T,rej,ksrej,num_sim,H0,
+                    NA_check=F,p_values_check=F) {
+
+  min_p_values<-c();Na<-c()
+  
+  for(f in 1:num_sim){
+    
+    # Data generation
+    res<-separate(people,sep_ratio,fake=!H0)
+    A<-res[[1]][,1:type_number]
+    B<-res[[2]][,1:type_number]
+    
+    ### Pearson`s Chi-square Tests on A & B ###
+    
+    if(pcs){
+    
+      # Test statistic generation
+      chi_sqr<-c()
+      for(i in 1:type_number){
+        chi_sqr<-c(chi_sqr,Q(list(A[,i],B[,i]),lo=20,up=80,sep=20))}
+      n<-test(20,80,20,chi_sqr)
+      
+      # Check number of NAs generated in each trial if necessary
+      Na<-c(Na,sum(is.na(chi_sqr)))
+      
+      # Judging whether to reject H0
+      if(n[[2]]=='Holm`s step-down'){
+        # Check minimum values of p-values if want to
+        min_p_values<-c(min_p_values,n[[1]])
+        if(n[[1]]<=(0.05/(type_number-sum(is.na(chi_sqr))))){rej[f]=1}
+      }
+      if(n[[2]]=='Hochberg`s step-up'){
+        if(n[[1]]>=1){rej[f]<-1}}
+    }
+    
+    ### Kolmogorov-Smirnov Tests on A & B ###
+    
+    if(ks){
+      ksps<-c()
+      for(l in 1:type_number){
+        ksp<-ks.test(A[,l],B[,l])$p.value
+        ksps<-c(ksps,ksp)}
+      if(min(c(na.omit(ksps)))<=(0.05/(type_number-sum(is.na(ksps))))){
+        ksrej[f]=1}}
+  }
+  
+  if(NA_check){print(Na)}
+  if(p_values_check){print(min_p_values)}
+  
+  result<-list(mean(rej),mean(ksrej))
+  names(result)<-c('Pearson`s Chi-square','Kolmogorov-Smirnov')
+  return(result)
+}
