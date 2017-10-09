5691dc39f6906824ee24e35db08d690deecb1669
小姐姐的憂鬱貓 <32256102+EricGraecum@users.noreply.github.com>
2017-10-09 21:25:19 +0800

    Update E-stat_H0T.R

--- E-stat_H0T.R
+++ E-stat_H0T.R
@@ -1,9 +1,9 @@
 source('simulate.R')
 source('supplement.R')
-
 library('energy')
-
 set.seed(1)
+
+# Version 1 #
 
 sep_ratio<-0.65
 source('Functions_E-stat_Version1.R')
@@ -16,3 +16,11 @@ num_sim<-100
 rej<-c()
 do_test(rej,num_sim,H0=T,B=99,package=F)
 # 0.05
+
+# Version 2 #
+
+sep_ratio<-0.65
+source('Functions_E-stat_Version2.R')
+num_sim<-100
+rej<-c()
+do_test_2(rej,num_sim,H0=T)
